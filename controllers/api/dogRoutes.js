23bed45bfe7faf48c4c36a8e141e9c40b4d3a3a0
CSillus <71670415+csills1416@users.noreply.github.com>
2023-08-29 19:58:08 -0400

    added delete, pull, post. Commented out. Future dev.

--- controllers/api/dogRoutes.js
+++ controllers/api/dogRoutes.js
@@ -58,7 +58,52 @@ router.get("/dogs/gender", async (req, res) => {
     res.status(500).json({ message: "An error occurred while fetching dog data by gender." });
   }
 });
+// This will be used for future development
+// // Create a new dog
+// router.post("/dogs", async (req, res) => {
+//   try {
+//     const newDog = await Dogs.create(req.body); 
+//     res.status(201).json(newDog);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(400).json({ message: "Bad request" });
+//   }
+// });
+
+// // Update a dog by ID
+// router.put("/dogs/:id", async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     const [rowsUpdated] = await Dogs.update(req.body, {
+//       where: { id },
+//     });
+//     if (rowsUpdated === 0) {
+//       res.status(404).json({ message: "Dog not found" });
+//       return;
+//     }
+//     res.json({ message: "Dog updated successfully" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(400).json({ message: "Bad request" });
+//   }
+// });
+
+// // Delete a dog by ID
+// router.delete("/dogs/:id", async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     const rowsDeleted = await Dogs.destroy({
+//       where: { id },
+//     });
+//     if (rowsDeleted === 0) {
+//       res.status(404).json({ message: "Dog not found" });
+//       return;
+//     }
+//     res.sendStatus(204);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// });
 
 module.exports = router;
-
-
