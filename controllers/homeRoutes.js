62842611507971c718273501606497494e41f2aa
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-08-31 23:16:39 -0400

    Adding same updates as reconcile-dev branch

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,21 @@
 const router = require("express").Router();
-const { User, Dog } = require("../models");
+const { Dogs, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 // Render the homepage
 router.get("/", async (req, res) => {
-  res.render("home");
+  res.render("home", { logged_in: req.session.logged_in });
 });
 
-// Route for passing in dog data to be rendered in the homepage
-router.get("/", async (req, res) => {
+// Pass the dog db data when rendering the Dogs for Adoption page
+router.get("/available_dogs", async (req, res) => {
   try {
-    // Get all dog data
     const dogData = await Dogs.findAll();
 
-    // Serialize data so the template can read it
-    const dogs = dogData.map((post) => post.get({ plain: true }));
+    const dogs = dogData.map((dog) => dog.get({ plain: true }));
 
-    // Pass serialized data and session flag into the homepage template
-    res.render("home", {
-      // dogs,
+    res.render("available_dogs", {
+      dogs,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -26,24 +23,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/available_dogs", async (req, res) => {
-  try {
-    console.log("Template rendering is working!");
-    // const dogData = await Dog.findAll();
-
-    // const dogTemplate = dogData.map((dog) => dog.get({ plain: true }));
-
-    res.render("available_dogs", {
-      // dogTemplate,
-      // logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Renders the adoption form application page
-// Use withAuth middleware to prevent access to route if user is not logged in
+// Use withAuth middleware to prevent access to route
 router.get("/application", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
@@ -62,15 +42,24 @@ router.get("/application", withAuth, async (req, res) => {
   }
 });
 
-// Renders the login page. If a route uses authentication to only render a page if the user is logged in, the withAuth function will redirect to this route
+// Render the login page if the user is NOT logged in. Otherwise, redirect to the homepage
 router.get("/login", (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+  // If the user is already logged in, redirect the request to the homepage
   if (req.session.logged_in) {
     res.redirect("/home");
     return;
   }
+  res.render("login", { logged_in: req.session.logged_in });
+});
 
-  res.render("login");
+// Render the "Under Construction" page when clicking on Sponsors in the navbar
+router.get("/sponsors", (req, res) => {
+  res.render("future_dev", { logged_in: req.session.logged_in });
+});
+
+// Render the "Under Construction" page when clicking on Volunteers in the navbar
+router.get("/volunteers", (req, res) => {
+  res.render("future_dev", { logged_in: req.session.logged_in });
 });
 
 module.exports = router;
