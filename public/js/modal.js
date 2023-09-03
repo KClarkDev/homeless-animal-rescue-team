94bdcf29e6a7851efb0703225b894acf9118cf16
Jamie Branch <schwung988@yahoo.com>
2023-09-03 17:00:02 -0400

    modal

---
+++ public/js/modal.js
@@ -0,0 +1,6 @@
+// // const myModal = document.getElementById('myModal')
+// // const myInput = document.getElementById('modal-content')
+
+// // myModal.addEventListener('shown.bs.modal', () => {
+// //   myInput.focus()
+// })
