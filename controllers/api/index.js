7570bb27efb6a875a52ce3e2a46f86329a3becc0
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-09-04 12:26:34 -0400

    Adding adoptionFormRoutes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const dogRoutes = require("./dogRoutes");
+const adoptionFormRoutes = require("./adoptionFormRoutes");
 
 router.use("/users", userRoutes);
 router.use("/dogs", dogRoutes);
+router.use("/adoption-form", adoptionFormRoutes);
 
 module.exports = router;
